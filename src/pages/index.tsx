1d9f079f091aaaee41a0400213e44445da769b92
Josh Mason <josh.mason.music@gmail.com>
2023-09-13 16:29:28 +1200

    move settings into its own page/route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,9 +25,7 @@ export default function Home() {
       <Generate onDataGenerated={handleDataGenerated} />
 
       <Bench playerData={playerData} />
-      <Link href="/settings">
-        <a>Go to settings</a>
-      </Link>
+      <Link href="/settings">Go to settings</Link>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8"></div>
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Settings } from "~/components/Settings";
 
-export function Settings() {
+export default function SettingsPage() {
   return (
-    <div>
-      <h1>SETTINGS</h1>
-    </div>
+    <>
+      <div>
+        <h1>SETTINGS</h1>
+      </div>
+      <Settings />
+    </>
   );
 }
